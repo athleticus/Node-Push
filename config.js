f06fdfad2c8cec8f6990ec627ad1c62129e456e8
Ben Martin <ben.martin@x-team.com>
2012-03-16 12:24:59 +1000

    Basic boilerplate.

---
+++ config.js
@@ -0,0 +1,12 @@
+var config = {};
+
+
+config.couchOpts = {
+    host: 'localhost',
+    port: 5984,
+    db: 'cache',
+    heartbeat: 10000
+};
+
+
+module.exports = config;
---
+++ node-push.js
@@ -0,0 +1,7 @@
+var http = require('http');
+var io = require('socket.io');
+
+//configuration settings
+var config = require('./config.js');
+
+console.log(config);
